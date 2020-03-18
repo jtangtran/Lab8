063976b1dd664ad7fefb5aa90ac7e41a808bf6cf
Jennifer <phuongtangtran@gmail.com>
2020-03-18 00:34:07 -0700

    finished lab 8 maybe

--- Lab8/ViewController.swift
+++ Lab8/ViewController.swift
@@ -12,14 +12,20 @@ import RealityKit
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
+    let boxAnchor = try! Experience.loadBox()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+  
         
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
     }
+    
+    @IBAction func buttonPress(_ sender: Any) {
+        boxAnchor.notifications.startNotification.post()
+    }
+    
 }
