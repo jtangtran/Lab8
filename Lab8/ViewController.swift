3b859424998f103adbd56511cbd0a34d6feffe23
Jennifer <phuongtangtran@gmail.com>
2020-03-17 22:39:29 -0700

    Initial Commit

---
+++ Lab8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab8
+//
+//  Created by Jennifer on 2020-03-17.
+//  Copyright © 2020 ics069. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
